4fe0e2d47d3eb942ac7610f0e59a24472233f077
evgeniievgenii671-debug <evgeniievgenii671@gmail.com>
2026-09-14 01:07:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import os
 import requests
 from fastapi import FastAPI, Request
-import google.generativeai as genai
+from google import genai
 
 app = FastAPI()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-if GEMINI_API_KEY:
-    genai.configure(api_key=GEMINI_API_KEY)
-
-# Настройка модели
-model = genai.GenerativeModel('gemini-1.5-flash')
+# Инициализация нового клиента GenAI
+client = genai.Client(api_key=GEMINI_API_KEY) if GEMINI_API_KEY else genai.Client()
 
 @app.post("/")
 async def handle_telegram_webhook(request: Request):
@@ -24,7 +21,7 @@ async def handle_telegram_webhook(request: Request):
         
         try:
             # Генерация ответа от Gemini
-            response = model.generate_content(user_message)
+            response = client.models.generate_content(model='gemini-2.5-flash', contents=user_message)
             bot_reply = response.text
         except Exception as e:
             bot_reply = f"Ошибка ИИ: {e}"
