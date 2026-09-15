920b49944c9bb7b78e212e862ea718947ef70680
evgeniievgenii671-debug <evgeniievgenii671@gmail.com>
2026-09-15 14:15:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ async def handle_telegram_webhook(request: Request):
                     "Content-Type": "application/json"
                 }
                 payload = {
-          "model": "llama-3.1-8b-instant",
+          "model": "gemma2-9b-it",
                     "messages": [
                         {"role": "user", "content": user_text}
                     ]
