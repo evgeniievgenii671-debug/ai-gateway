b5c7c89d2dd97db3c6686cf909ee7feb8308e01f
evgeniievgenii671-debug <evgeniievgenii671@gmail.com>
2026-09-15 21:49:46 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
 import json
-import time
 from fastapi import FastAPI, Request
 import httpx
 from google import genai
 
 app = FastAPI()
 
-client = genai.Client()
+client = genai.Client(api_key="AQ.Ab8RN6IKsmLU-6WJ2uB14wXBY-5eQBF9s5j7u1-jZJzt2aGBYg")
 
 TELEGRAM_BOT_TOKEN = "8680814733:AAGUbD-eHtDXy7XyR4N2TpEQmdk0vYX_B8M"
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
@@ -24,13 +23,14 @@ async def telegram_webhook(request: Request):
         
         try:
             response = client.models.generate_content(
-                model="models/gemini-1.5-flash",
+                model="gemini-2.5-flash",
                 contents=user_text,
             )
             if response and response.text:
                 reply_text = response.text
         except Exception as e:
-            print(f"Ошибка: {e}")
+            print(f"ПОДРОБНАЯ ОШИБКА: {e}")
+            reply_text = f"Ошибка: {str(e)}"
 
         async with httpx.AsyncClient() as httpx_client:
             await httpx_client.post(
