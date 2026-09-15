18d77941eb04080a1e79846caaaf7926d685806c
evgeniievgenii671-debug <evgeniievgenii671@gmail.com>
2026-09-15 11:47:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def handle_telegram_webhook(request: Request):
         user_message = data["message"]["text"]
         
         response = client.models.generate_content(
-            model='gemini-2.0-flash',
+            model='gemini-2.5-flash',
             contents=user_message
         )
         reply_text = response.text
